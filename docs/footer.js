b42e6f4f5f48b1a95a65ade03733ae45bb59aaad
yuki <akaiyukiusagi@gmail.com>
2026-01-25 23:37:49 +0900

    文言追加

--- docs/footer.js
+++ docs/footer.js
@@ -1,10 +1,12 @@
 (() => {
   const footerHtml = `
     各ツールは制作中です。順次修正・追加予定。
-     <br>
+    <br>
     もしよければ
-     <a href="https://www.amazon.jp/hz/wishlist/ls/193UZ703SCTQJ?ref_=wl_share" target="_blank" rel="noopener">欲しいものリスト</a> 
+    <a href="https://www.amazon.jp/hz/wishlist/ls/193UZ703SCTQJ?ref_=wl_share" target="_blank" rel="noopener">欲しいものリスト</a>
     から支援していただけると、アップデートのモチベーションになります。
+    <br>
+    ※本ツールはファンによって製作された非公式のもので、運営・開発元とは一切関係ありません。
   `.trim();
 
   const main = document.querySelector("main");
